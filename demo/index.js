f814f65841cd7ba3675f8c268ff29732708ea1f7
Nikolaj <abonner.et@gmail.com>
2022-10-06 21:46:06 +0200

    Updated

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,12 @@
 import { Modules } from '../rockjs/rock.js'
 
-Modules().btn()
+
+Modules.btn("btn1" ,{
+    title: "Click Me",
+})
+
+Modules.blockquote("bq1" ,{
+    text: "Hello my name is Nikolaj :)<br>I am 18 years old",
+
+})
+
